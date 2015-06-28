1c37d65f30d0ab82e59cb1f7db8350dc2e7e6e78
shufanshijie <314148920@qq.com>
2015-06-28 21:52:07 +0800

    修改测试用例

--- src/main/test/com/shufan/usercenter/test/TestProductDao.java
+++ src/main/test/com/shufan/usercenter/test/TestProductDao.java
@@ -1,14 +1,20 @@
 package com.shufan.usercenter.test;
 
 import haiyan.common.config.PathUtil;
+import haiyan.common.exception.Warning;
 import haiyan.common.intf.database.orm.IDBRecord;
 import haiyan.common.intf.database.orm.IDBResultSet;
 import haiyan.orm.database.DBPage;
+import haiyan.orm.database.DBRecord;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.shufan.product.common.ContextListener;
 import com.shufan.product.dao.ProductDao;
@@ -41,18 +47,39 @@ public class TestProductDao {
 
 	private static void testBill() throws Throwable {
 		ProductDao dao = new ProductDaoImpl(null);
-
-//		IDBBill bill = dao.createSetMeal();
-		IDBResultSet headSet = new DBPage(new ArrayList<IDBRecord>());
-		IDBRecord headRecord = headSet.appendRow();
-		headRecord.set("NAME", "套餐5");
-		headRecord.set("PRICE", 10);
-		headRecord.set("INTRODUCTION", "好吃不贵5");
-		Date date = new Date();
-		headRecord.set("DATE", date);
-		headRecord.set("WEEK", "周日");
-		
-		
+		Calendar cal = Calendar.getInstance();
+		int size = 5;
+		for(int i=0;i<size;i++){
+			cal.set(Calendar.DAY_OF_MONTH, cal.get(Calendar.DAY_OF_MONTH)+1);
+			Date date = cal.getTime();
+			int day = date.getDay();
+			String week = null;
+			if(day == 1){
+				week = "周一";
+			}else if(day == 2)
+				week = "周二";
+			else if(day == 3)
+				week = "周三";
+			else if(day == 4)
+				week = "周四";
+			else if(day == 5)
+				week = "周五";
+			else if(day == 6)
+				week = "周六";
+			else 
+				week = "周日";
+			IDBRecord record = new DBRecord();
+			record.set("NAME", "套餐"+i);
+			record.set("OLDPRICE", "10");
+			record.set("PRICE", "8");
+			record.set("DATE", date);
+			record.set("WEEK", week);
+			try {
+				dao.addMeal(record);
+			} catch (Throwable e) {
+				throw new Warning(500,e);
+			}
+		}
 	}
 
 
